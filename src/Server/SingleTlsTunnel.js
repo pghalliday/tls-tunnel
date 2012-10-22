0a3932ef6413d0eb0615830a5585cf6b40b4177b
Peter Halliday <pghalliday@gmail.com>
2012-10-22 03:28:15 +0200

    Multiplexing over a single socket

--- src/Server/SingleTlsTunnel.js
+++ src/Server/SingleTlsTunnel.js
@@ -1,9 +1,44 @@
-var tls = require('tls');
+var tls = require('tls'),
+    crypto = require('crypto'),
+    net = require('net'),
+    Multiplex = require('../util/Multiplex');
 
-function SingleTlsTunnel(options) {
-  var server = tls.createServer(options);
-  var self = this;
+function SingleTlsTunnel(tlsOptions) {
+  var self = this,
+      multiplex,
+      clientConnected = false;
   
+  var server = net.createServer(function(connection) {
+    if (!clientConnected) {
+      // This is the first connection so it should be a client trying to
+      // negotiate a secure session
+      var securePair = tls.createSecurePair(
+        crypto.createCredentials({
+          key: tlsOptions.key,
+          cert: tlsOptions.cert,
+          ca: tlsOptions.ca
+        }),
+        true,
+        tlsOptions.requireCert,
+        tlsOptions.rejectUnauthorized
+      );
+      var cleartext = securePair.cleartext,
+          encrypted = securePair.encrypted;
+      
+      multiplex = new Multiplex();
+      connection.pipe(encrypted).pipe(connection);
+      cleartext.pipe(multiplex).pipe(cleartext);
+      
+      clientConnected = true;  
+      connection.on('end', function() {
+        clientConnected = false;
+      });
+    } else {
+      var tunnel = multiplex.createStream();
+      connection.pipe(tunnel).pipe(connection);
+    }
+  });
+    
   self.listen = function(port, callback) {
     server.listen(port, callback);
   };
